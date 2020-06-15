7c49bc8a2a5a014c5fd1ba260e3cd46b20a6d991
Heath Mast <hrmast@gmail.com>
2020-06-15 00:40:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,25 +1,20 @@
 class Anagram
-attr_accessor :word
-
-def initialize(word)
-  @word = word
-end
-
-def match(words_to_compare)
-  words_to_compare.each do |word|
-    if words_to_compare.split().sort = word
-      return word
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array_of_words)
+    array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
     end
-
+  end
+  
+  
 end
 
-#How will you determine if one word is an anagram for another?
-#You'll need to iterate over the array of words that the .match method takes as an argument.
-#You will compare each word of that array to the word that the Anagram class is initialized with.
-#To determine if they are anagrams, try determining if they are composed of the same letters.
-#Remember that you can split a word into an array of letters using some_word.split("").
-#You can compare two arrays using the ==. For example:
-
-
-end
-end
+listen = Anagram.new("listen")
+#listen.match(%w(enlists))
+listen.match(%w(enlists google inlets banana))
